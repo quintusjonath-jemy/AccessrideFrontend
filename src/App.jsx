44e35e96290af37525058fa6b3f8df7ce859a385
Kabil <kulamkabil07@gmail.com>
2026-06-10 11:16:20 +0530

    drivertips page

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import Dashboard from "./admin/pages/Dashboard";
 import Drivers from "./admin/pages/Drivers";
 import Navigation from "./admin/pages/Navigation";
 import Rides from "./admin/pages/Rides";
-import RidePage from "./ridepage/RidePage";
+import RidePage from "./Ridepage/RidePage";
 import DriverDashboard from "./Driverdashboard/DriverDashboard";
+import DriverTrips from "./Driverdashboard/DriverTrips";
+import Earnings from "./Driverdashboard/Earnings";
 import EmergencySOS from "./Emergency/EmergencySOS";
 import Settings from "./admin/pages/Settings";
 import NotificationSettings from "./admin/pages/settings/NotificationSettings";
@@ -36,6 +38,8 @@ const App = () => {
           <Route path="/rides" element={<Rides />} />
           <Route path="/ride" element={<RidePage />} />
           <Route path="/driver-dashboard" element={<DriverDashboard />} />
+          <Route path="/driver-trips" element={<DriverTrips />} />
+          <Route path="/driver-earnings" element={<Earnings />} />
           <Route path="/emergency" element={<EmergencySOS />} />
 
           <Route path="/my-rides" element={<MyRides />} />
