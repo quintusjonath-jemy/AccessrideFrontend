bbf0172948d5bcfd36a5426b68a5caaf805bd802
quintusjonath-jemy <quintusjonath2107@gmail.com>
2026-06-16 22:28:31 +0530

    modified clock and statscard

--- src/admin/components/StatsCard.jsx
+++ src/admin/components/StatsCard.jsx
@@ -1,6 +1,6 @@
 const StatsCard = ({ title, value, color }) => {
   return (
-    <div className="bg-white dark:bg-slate-800 p-5 rounded-2xl shadow-md hover:shadow-xl border dark:border-slate-700 transition">
+    <div className="bg-white dark:bg-slate-800 p-5 rounded-2xl shadow-md hover:shadow-xl dark:border-slate-700 transition">
 
       <h2 className="text-gray-500 dark:text-slate-400 text-sm">
         {title}
