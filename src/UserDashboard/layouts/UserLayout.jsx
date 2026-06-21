ffae558b5f2fa47a7979c1360b3ad25828c9ca0f
Kabil <kulamkabil07@gmail.com>
2026-06-21 21:09:39 +0530

    SOS layout change

--- src/UserDashboard/layouts/UserLayout.jsx
+++ src/UserDashboard/layouts/UserLayout.jsx
@@ -3,13 +3,14 @@ import BottomNavigation from "../components/BottomNavigation";
 
 const UserLayout = () => {
   const location = useLocation();
-  const isSOSPage = location.pathname === "/user/sos";
+  const hideFooterPages = ["/user/ride"];
+  const shouldHideFooter = hideFooterPages.includes(location.pathname);
 
   return (
-    <div className={`min-h-screen bg-slate-100 ${!isSOSPage ? "pb-16" : ""}`}>
+    <div className={`min-h-screen bg-slate-100 ${!shouldHideFooter ? "pb-16" : ""}`}>
       <Outlet />
 
-      {!isSOSPage && (
+      {!shouldHideFooter && (
         <div className="fixed bottom-0 left-0 w-full">
           <BottomNavigation />
         </div>
