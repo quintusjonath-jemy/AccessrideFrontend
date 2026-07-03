a46cc5989c1ae428d065b91cadbfb499ccd4c698
nithan11 <nithanrajkumar0237@gmail.com>
2026-07-03 18:13:14 +0530

    add the profilepage in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,7 @@ const App = () => {
           <Route path="booking" element={<BookingPage />} />
           <Route path="schedule" element={<SchedulePage />} />
           <Route path="ride" element={<RideTrackingPage />} />
+          <Route path="/user/profile" element={<UserProfilePage />} />
           <Route path="sos" element={<EmergencySOS />} />
           <Route path="history" element={<HistoryPage />} />
         </Route>
@@ -109,7 +110,6 @@ const App = () => {
         {/* standalone / shared routes */}
         <Route path="/ride" element={<RidePage />} />
         <Route path="/emergency" element={<EmergencySOS />} />
-        <Route path="/profile" element={<UserProfilePage />} />
         <Route path="/complete-ride" element={<CompleteRidePage />} />
       </Routes>
     </BrowserRouter>
