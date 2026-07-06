305fd77e8ce414960224c29839e55d66cae39948
Kabil <kulamkabil07@gmail.com>
2026-07-06 18:54:32 +0530

    ride page for driver

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,6 @@ const App = () => {
 
         {/* standalone / shared routes */}
         <Route path="/ride" element={<RidePage />} />
-        <Route path="/emergency" element={<EmergencySOS />} />
         <Route path="/profile" element={<UserProfilePage />} />
         <Route path="/complete-ride" element={<CompleteRidePage />} />
       </Routes>
